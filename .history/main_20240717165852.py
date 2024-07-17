0ce8f60efc153a5d76dcc41f56e1e8860179826f
prithika12 <prithikamalesan@gmail.com>
2024-07-17 17:20:34 -0400

    installed statsmodel

---
+++ .history/main_20240717165852.py
@@ -0,0 +1,10 @@
+import streamlit as st # python framework used to create web apps
+from datetime import date
+
+import yfinance as yf # python library to fetch financial data from Yahoo Finance
+from prophet import Prophet # prophet module for time series forecasting
+from prophet.plot import plot_plotly # module to visualize Prophet forecasts using Plotly
+from plotly import graph_objs as go # library as go to create interactive graphs
+
+START = "2012-01-01" #data starting from this date
+TODAY = date.today().strftime("%Y-%m-%d") #data upto today 
